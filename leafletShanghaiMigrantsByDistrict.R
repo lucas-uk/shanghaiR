6c795ae03ee739753adad1dfbfe2b48ef2e78330
Julian Rosser <julianrosser@gmail.com>
2017-05-08 17:18:21 +0100

    revised map creation

--- leafletShanghaiMigrantsByDistrict.R
+++ leafletShanghaiMigrantsByDistrict.R
@@ -2,30 +2,31 @@
 #install.packages("rgdal")
 #install.packages("rgeos")
 
+setwd("R_scripts/r_mapping/shanghaiR")
+
+
 #load mapping and spatial data libraries
 library(leaflet) 
 library(rgdal)
 library(rgeos)
 library(classInt)
-
+library(raster)
 
 getwd() #get current R working directory. change with setwd("C:/Users/myusername")
 
-
 nClassBreaks = 6
 classifierFunction = "jenks"
-
 
 #read in the GIS polygon shapefile of the adm1 data. Assumes data is in directory CHN_adm_shp
 boundaries <- readOGR("../CHN_adm_shp/Shanghai_AL6_edit_for_Minhang.shp", layer = "Shanghai_AL6_edit_for_Minhang", verbose = FALSE)
 
 #Geometry is quite detailed so they can be simplified to make them display quicker
-simplifiedPolygons <- gSimplify(boundaries, 0.001, topologyPreserve=TRUE) #Creates simplified polygons
+#simplifiedPolygons <- gSimplify(boundaries, 0.001, topologyPreserve=TRUE) #Creates simplified polygons
+simplifiedPolygons = boundaries
 chinaSPDF = SpatialPolygonsDataFrame(simplifiedPolygons, data=boundaries@data) #Need to copy the attributes from the loaded data into 
-
 simplifiedPolygons = spTransform(simplifiedPolygons, CRS("+proj=utm +zone=51 +datum=WGS84"))
 
-areas = gArea(simplifiedPolygons,byid = TRUE) #project, please
+chinaSPDF$areas = gArea(simplifiedPolygons,byid = TRUE)/1000000 #sqkm
 
 #Create a map object with some background OpenStreetMap
 myMap = leaflet(chinaSPDF) %>% addTiles() 
@@ -43,26 +44,16 @@ chinaSPDF <- merge(chinaSPDF,migData, by.x="ID", by.y="OSMID")
 #Count variable
 myVariableToMap = chinaSPDF@data$Number_of_migrants
 #Add a normalised by area variable
-migData$migsNormalisedByArea = migData$Number_of_migrants/areas
-myVariableToMap2 = migData$migsNormalisedByArea
+chinaSPDF$migsNormalisedByArea = chinaSPDF$Number_of_migrants/chinaSPDF$areas
+myVariableToMap2 = chinaSPDF$migsNormalisedByArea
 
 #Classlify the data for choropleth mapping
-palData = classIntervals(migData$Number_of_migrants, n=nClassBreaks, style=classifierFunction)
+palData = classIntervals(chinaSPDF$Number_of_migrants, n=nClassBreaks, style=classifierFunction)
 
-palData2 = classIntervals(migData$migsNormalisedByArea, n=nClassBreaks, style=classifierFunction)
+palData2 = classIntervals(chinaSPDF$migsNormalisedByArea, n=nClassBreaks, style=classifierFunction)
 
 
 #create a map palette style for the choropleth colour
-pal <- colorNumeric(
-  palette = "Greens",
-  domain = myVariableToMap
-)
-
-pal2 <- colorNumeric(
-  palette = "Greens",
-  domain = myVariableToMap2
-)
-
 
 #paletteColourFunction <- colorBin("YlOrRd", domain = myVariableToMap, bins = bins)
 paletteColourFunction <- colorBin("Reds", domain = myVariableToMap, bins = palData$brks)
@@ -70,26 +61,40 @@ paletteColourFunction2 <- colorBin("Reds", domain = myVariableToMap2, bins = pal
 
 
 
+
+
+
+################### Map display labels ###############################
+
 layerTitle = "Number of migrants"
 layerTitle2 = "Number of migrants by area"
 
 labels <- sprintf(
-  "<strong>%s</strong><br/>%g migrants",
-  migData$Districts_and_Town, migData$Number_of_migrants
+  "%s</strong><br/>%g migrants",
+  chinaSPDF$Districts_and_Town, chinaSPDF$Number_of_migrants
 ) %>% lapply(htmltools::HTML)
 
+labels2 <- sprintf(
+  "%s</strong><br/>Density: %g migrants /km<sup>2</sup><br/>Num of migrants: %s",
+  chinaSPDF$Districts_and_Town, chinaSPDF$migsNormalisedByArea, chinaSPDF$Number_of_migrants
+) %>% lapply(htmltools::HTML)
+
+
+
+
+
+######################## Map object creation ######################
+
+# Put both datasets maps
 myMap %>%
-  #addProviderTiles("Stamen.Toner") %>%
-  #addProviderTiles("CartoDB.Positron") %>%
-  #addProviderTiles("ESRI.WorldImagery") %>%
-  
-  addProviderTiles("OpenStreetMap.BlackAndWhite", group = "OSM basemap (CartoDB Positron)") %>%
+  addProviderTiles("OpenStreetMap.BlackAndWhite") %>%
   
   addPolygons(
      fillOpacity = 0.60, smoothFactor = 0.5,
      fillColor = ~paletteColourFunction(myVariableToMap),
      weight = 2,
      dashArray = "1",
+     color ="white",
      group = layerTitle,
      label=labels,
      highlight = highlightOptions(
@@ -104,14 +109,14 @@ myMap %>%
       direction = "auto")) %>%  
     
   
-  addLegend("topright", pal = paletteColourFunction, values = ~myVariableToMap,
+  addLegend("bottomright", pal = paletteColourFunction, values = ~myVariableToMap,
             title = layerTitle,
             labFormat = labelFormat(prefix = ""),
             opacity = 1
   )%>%
 
   
-  addLegend( pal = paletteColourFunction2, values = ~myVariableToMap2,
+  addLegend( "bottomright", pal = paletteColourFunction2, values = ~myVariableToMap2,
              title = layerTitle2,
              opacity = 1
   ) %>%
@@ -122,8 +127,8 @@ myMap %>%
     weight = 2,
     dashArray = "1",
     color = "white",
-    group = layerTitle,
-    label=labels,
+    group = layerTitle2,
+    label=labels2,
     highlight = highlightOptions(
       weight = 5,
       color = "#666",
@@ -141,8 +146,6 @@ myMap %>%
     overlayGroups = c(layerTitle, layerTitle2),
     options = layersControlOptions(collapsed = FALSE)
   )
-
-
   #Viewer window should now display choropleth map.
 
 
@@ -155,38 +158,19 @@ myMap %>%
 
 
 
-
-
-
-
-labels2 <- sprintf(
-  "<strong>%s</strong><br/>%g migrants / m<sup>2</sup>",
-  migData$Districts_and_Town, migData$migsNormalisedByArea
-) %>% lapply(htmltools::HTML)
-
-
-myMap2 = leaflet(chinaSPDF) %>% addTiles() 
-
-myMap2 %>%
-  #addProviderTiles("Stamen.Toner") %>%
-  #addProviderTiles("CartoDB.Positron") %>%
-  #addProviderTiles("ESRI.WorldImagery") %>%
-  
-  addProviderTiles("OpenStreetMap.BlackAndWhite", group = "OSM basemap (CartoDB Positron)") %>%
-
-  addLegend( pal = paletteColourFunction2, values = ~myVariableToMap2,
-             title = layerTitle2,
-             opacity = 1
-  ) %>%
+myMapCounts = leaflet(chinaSPDF) %>% addTiles() 
+# Separate map for counts
+myMapCounts %>%
+  addProviderTiles("OpenStreetMap.BlackAndWhite", group = "OSM basemap") %>%
   
   addPolygons(
     fillOpacity = 0.60, smoothFactor = 0.5,
-    fillColor = ~paletteColourFunction2(myVariableToMap2),
+    fillColor = ~paletteColourFunction(myVariableToMap),
     weight = 2,
-    dashArray = "1",
     color = "white",
+    dashArray = "1",
     group = layerTitle,
-    label=labels2,
+    label=labels,
     highlight = highlightOptions(
       weight = 5,
       color = "#666",
@@ -199,10 +183,52 @@ myMap2 %>%
       direction = "auto")) %>%  
   
   
-  addLayersControl(
-    #baseGroups = c("OSM basemap (CartoDB Positron)", "OSM basemap"),
-    options = layersControlOptions(collapsed = FALSE)
+  addLegend("bottomright", pal = paletteColourFunction, values = ~myVariableToMap,
+            title = layerTitle,
+            labFormat = labelFormat(prefix = ""),
+            opacity = 1
   )
 
+#Viewer window should now display choropleth map.
+
+
+
+
+
+
+
+
+
+
+# Separate map for density
+myMapDensity = leaflet(chinaSPDF) %>% addTiles() 
+myMapDensity %>%
+  addProviderTiles("OpenStreetMap.BlackAndWhite") %>%
+  
+  addLegend( "bottomright", pal = paletteColourFunction2, values = ~myVariableToMap2,
+             title = layerTitle2,
+             opacity = 1
+  ) %>%
+  
+  addPolygons(
+    fillOpacity = 0.60, smoothFactor = 0.5,
+    fillColor = ~paletteColourFunction2(myVariableToMap2),
+    weight = 2,
+    dashArray = "1",
+    color = "white",
+    group = layerTitle2,
+    label=labels2,
+    highlight = highlightOptions(
+      weight = 5,
+      color = "#666",
+      dashArray = "",
+      fillOpacity = 0.7,
+      bringToFront = TRUE),
+    labelOptions = labelOptions(
+      style = list("font-weight" = "normal", padding = "3px 8px"),
+      textsize = "15px",
+      direction = "auto")) 
 
 #Viewer window should now display choropleth map.
+
+
